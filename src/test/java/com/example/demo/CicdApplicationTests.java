07d2dc8a00b0f7d9a63d15ff863c061d0bea42f6
amolpatil <amolrpatil2012@gmail.com>
2023-09-17 09:35:55 +0530

    Added Test Case in Application

--- src/test/java/com/example/demo/CicdApplicationTests.java
+++ src/test/java/com/example/demo/CicdApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Testing ");
+		assertEquals(true, true);
 	}
 
 }
